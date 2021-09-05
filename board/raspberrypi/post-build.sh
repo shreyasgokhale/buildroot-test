7e08dc7dce24d6adc7d4945382d856b6b3be3bb6
Shreyas Gokhale <shreyas6gokhale@gmail.com>
2021-09-05 19:05:11 +0200

    STA: Can connect to hotspot AP: Not working with address assignment

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,7 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
+chmod +600 ${TARGET_DIR}/etc/mdev.conf
